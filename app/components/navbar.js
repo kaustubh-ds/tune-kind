9e79132a104f6e4de74bfadfd58711a340416b59
Kaustubh <salunkhekaustubh2004@gmail.com>
2025-05-11 09:47:21 +0530

    Updated navbar

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -18,10 +18,16 @@ export default function Navbar() {
 
   return (
     <nav className="bg-white text-black w-full px-6 py-4 flex justify-between items-center shadow shadow-[0_0_15px_#78aedb] sticky top-0 z-10">
-      <Link href="/">
-        <Image src="/tklogo.png" alt="TuneKind Logo" width={125} height={10} className="rounded-sm" />
+      <Link href="/" className="flex items-center space-x-1">
+        <Image src="/tkicon.png" alt="TuneKind Icon" width={30} height={45} className="" />
+        <Image src="/tklogo.png" alt="TuneKind Logo" width={100} height={45} className="rounded-sm" />
       </Link>
       <div className="space-x-4 hidden md:flex">
+        <a href="/" className="relative group overflow-hidden text-black">
+          Home
+          <span className="absolute left-0 bottom-0 h-[2px] w-full bg-[#78aedb] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center"></span>
+        </a>
+        <p>|</p>
         <a href="/about" className="relative group overflow-hidden text-black">
           About
           <span className="absolute left-0 bottom-0 h-[2px] w-full bg-[#78aedb] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center"></span>
@@ -36,13 +42,13 @@ export default function Navbar() {
             <p>|</p>
             <button onClick={handleLogout} className="relative group overflow-hidden text-black">
               Logout
-              <span className="absolute left-0 bottom-0 h-[2px] w-full bg-red transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center"></span>
+              <span className="absolute left-0 bottom-0 h-[2px] w-full bg-[#ff0000] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center"></span>
             </button>
           </>
         ) : (
           <>
             <p>|</p>
-            <a href="/login" className="relative group overflow-hidden text-black">
+            <a href="/login" className="relative group overflow-hidden text-black pb-0.5">
               Login
               <span className="absolute left-0 bottom-0 h-[2px] w-full bg-[#78aedb] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center"></span>
             </a>
